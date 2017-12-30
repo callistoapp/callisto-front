da537d803365bed3cb92991af44b0736ae4c9fc9
cmondion <clement.mondion@clicrdv.com>
2017-12-30 15:28:41 +0100

    add new routes

--- src/router.js
+++ src/router.js
@@ -4,9 +4,11 @@
 
 import React from 'react';
 import Projects from './components/projects';
+import NewProject from './components/new_project';
 import Home from './components/home';
 import ProjectHome from './components/project/home';
 import Tasks from './components/project/tasks';
+import NewTask from './components/project/new_task';
 import Releases from './components/project/releases';
 import App from './App';
 import {Router, Route, Switch} from 'react-router'
@@ -19,8 +21,10 @@ const AppRouter = () => (
         <App>
             <Route exact path="/" component={Home}/>
             <Route path="/projects" component={Projects}/>
+            <Route path="/new_project" component={NewProject}/>
             <Route path="/:project/home" component={ProjectHome}/>
             <Route path="/:project/tasks" component={Tasks}/>
+            <Route path="/:project/new_task" component={NewTask}/>
             <Route path="/:project/releases" component={Releases}/>
         </App>
     </Router>
