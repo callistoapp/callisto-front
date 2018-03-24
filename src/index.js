a77ee2253f74b01fd1f914d6cc5a0315febfeca6
cmondion <clement.mondion@clicrdv.com>
2018-03-24 14:20:25 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './router';
 import registerServiceWorker from './registerServiceWorker';
-import { ApolloClient, createNetworkInterface } from 'react-apollo';
+import { ApolloClient } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
-    networkInterface: createNetworkInterface({
-        uri: 'http://localhost:8080/graphql',
-    }),
+    link: new HttpLink({ uri: 'https://api.callisto.com/graphql', credentials: "include" }),
+    cache: new InMemoryCache()
 });
 
 ReactDOM.render(
