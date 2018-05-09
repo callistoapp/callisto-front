2890c8b72619fbbe08b36a5e33487bf03b4ec0e4
cmondion <clement.mondion@clicrdv.com>
2018-05-09 10:59:33 +0200

    feat(401 redirect): add error catching for 401 errors to redirect to auth

--- src/index.js
+++ src/index.js
@@ -2,14 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './router';
-import registerServiceWorker from './registerServiceWorker';
-import { ApolloClient } from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { onError } from "apollo-link-error";
 import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { ApolloClient } from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import registerServiceWorker from './registerServiceWorker';
+
+const httpLink = new HttpLink({ uri: 'http://api.callisto.com/graphql', credentials: "include" });
+
+const errorLink = onError(({ response, graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+
+  if (networkError.statusCode === 401)
+    window.location = "http://auth.callisto.com/login"
+
+});
+
+const link = ApolloLink.from([
+  errorLink,
+  httpLink,
+]);
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: 'https://api.callisto.com/graphql', credentials: "include" }),
+    link,
     cache: new InMemoryCache()
 });
 
