0b1dc86e91ef66bd0dcfe92ad33fae8b2eafb7b2
cmondion <clement.mondion@clicrdv.com>
2018-05-19 08:31:20 +0200

    feat(style): popups reworked

--- src/components/popups/new_project.js
+++ src/components/popups/new_project.js
@@ -5,28 +5,43 @@
 import React, {Component} from 'react';
 import * as _ from 'lodash';
 import {
-  Dialog, AppBar, Toolbar, IconButton, Typography, Snackbar, TextField, FormControl, InputLabel, Button
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  IconButton,
+  Typography,
+  Snackbar,
+  TextField,
+  FormControl,
+  InputLabel,
+  Button
 } from 'material-ui'
-import {Close} from '@material-ui/icons'
+import {Close, Delete} from '@material-ui/icons'
 import {graphql} from 'react-apollo';
 import {gql} from 'apollo-boost';
-import { withStyles } from 'material-ui/styles';
+import {withStyles} from 'material-ui/styles';
+import {withRouter} from 'react-router';
 
 
 const styles = theme => ({
-  fab: {
+  fab     : {
     position: 'absolute',
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 2,
+    bottom  : theme.spacing.unit * 2,
+    right   : theme.spacing.unit * 2,
   },
-  flex: {
+  flex    : {
     flex: 1,
   },
-  paper: {
+  paper   : {
     width: 330
   },
   centered: {
     textAlign: 'center'
+  },
+  topMarged: {
+    marginTop: 20
   }
 });
 
@@ -49,19 +64,36 @@ class NewProject extends Component {
       variables: {name, url, description, repository, statuses}
     })
         .then(({data}) => {
-          this.setState({loading: false, error: ''});
-          this.props.history.push(`/${name}/home`)
+          this.setState({
+            name       : '',
+            url        : '',
+            description: '',
+            repository : '',
+            statuses   : ['backlog', 'wip', 'verify', 'done'],
+            loading    : false,
+            error      : ''
+          });
+          this.props.history.push({
+            pathname: `/p/dashboard`,
+            search: `?project_id=${data.createProject.id}`
+          });
+          this.props.handleClose();
         }).catch((error) => {
       this.setState({loading: false, error});
     });
 
   };
 
-  updateCategory = name => e => {
+  updateCategory = i => e => {
     let statuses = _.assign([], this.state.statuses);
-    const i = name.substr(name.length - 1);
-    statuses[i] = e.target.value;
+    statuses[i]  = e.target.value;
     this.setState({statuses})
+  };
+
+  deleteCategory = name => {
+    this.setState({
+      statuses: _.without(this.state.statuses, name)
+    })
   };
 
   addCategory = () => {
@@ -72,7 +104,7 @@ class NewProject extends Component {
 
   constructor(props) {
     super(props);
-    this.state        = {
+    this.state          = {
       name       : '',
       url        : '',
       description: '',
@@ -81,78 +113,92 @@ class NewProject extends Component {
       loading    : false,
       error      : ''
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange   = this.handleChange.bind(this);
+    this.handleSubmit   = this.handleSubmit.bind(this);
     this.updateCategory = this.updateCategory.bind(this);
-    this.addCategory = this.addCategory.bind(this);
+    this.addCategory    = this.addCategory.bind(this);
   }
 
   render() {
-    const {classes} = this.props;
+    const {classes}                                      = this.props;
     const {name, url, description, repository, statuses} = this.state;
     return (
-      <Dialog open={this.props.open}>
-        <AppBar>
-          <Toolbar>
-            <IconButton color="inherit" onClick={this.props.handleClose} aria-label="Close">
-              <Close />
-            </IconButton>
-            <Typography variant="title" color="inherit" className={classes.flex}>
-              Create new project
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <form onSubmit={this.handleSubmit}>
-          {this.state.error && <Snackbar
-            error
-            header='Error occurred'
-            content={this.state.error}
-          />}
-          <TextField
-            label={'Name'}
-            name='name'
-            value={name}
-            placeholder='Name of the project'
-            onChange={this.handleChange('name')}
-          />
-          <TextField
-            label={'Description'}
-            name='description'
-            value={description}
-            placeholder='Describe the project objectives'
-            onChange={this.handleChange('description')}
-          />
-          <TextField
-            label={'Repository'}
-            name='repository'
-            value={repository}
-            placeholder='The Url of the repository hosing the source code'
-            onChange={this.handleChange('repository')}
-          />
-          <TextField
-            label={'Url'}
-            name='url'
-            value={url}
-            placeholder='The url of the desired website'
-            onChange={this.handleChange('url')}
-          />
-          <FormControl>
-            <InputLabel>Board categories</InputLabel>
-            {_.map(statuses, (status, i) => (
-              <TextField
-                key={i}
-                name={`status${i}`}
-                value={status}
-                onChange={this.updateCategory}
-              />
-            ))}
-            <Button secondary onClick={this.addCategory}>New category</Button>
-          </FormControl>
-          <Button type='submit'>Submit</Button>
-        </form>
+      <Dialog open={this.props.open} aria-labelledby="form-dialog-title" onClose={this.props.handleClose}>
+        <DialogTitle id="form-dialog-title">Create new project</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Define the details of the projects
+          </DialogContentText>
+
+          <form onSubmit={this.handleSubmit}>
+            {this.state.error && <Snackbar
+              error
+              header='Error occurred'
+              content={this.state.error}
+            />}
+            <TextField
+              fullWidth
+              label={'Name'}
+              name='name'
+              value={name}
+              placeholder='Name of the project'
+              onChange={this.handleChange('name')}
+            />
+            <TextField
+              fullWidth
+              label={'Description'}
+              name='description'
+              value={description}
+              placeholder='Describe the project objectives'
+              onChange={this.handleChange('description')}
+            />
+            <TextField
+              fullWidth
+              label={'Repository'}
+              name='repository'
+              value={repository}
+              placeholder='The Url of the repository hosing the source code'
+              onChange={this.handleChange('repository')}
+            />
+            <TextField
+              fullWidth
+              label={'Url'}
+              name='url'
+              value={url}
+              placeholder='The url of the desired website'
+              onChange={this.handleChange('url')}
+            />
+            <div className={classes.topMarged}>
+              <InputLabel>Board categories</InputLabel>
+              <br />
+                {_.map(statuses, (status, i) => (
+                  <div>
+                    <TextField
+                      key={i}
+                      name={`status${i}`}
+                      value={status}
+                      onChange={this.updateCategory(i)}
+                    />
+                    <IconButton
+                      className={classes.button}
+                      aria-label="Delete"
+                      onClick={() => this.deleteCategory(status)}
+                    >
+                      <Delete />
+                    </IconButton>
+                  </div>
+                ))}
+              <Button color={'primary'} onClick={this.addCategory}>New category</Button>
+            </div>
+            <DialogActions>
+              <Button color="secondary" onClick={this.props.handleClose}>Cancel</Button>
+              <Button type='submit' color="primary">Submit</Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
       </Dialog>
     );
   }
 }
 
-export default WithData(withStyles(styles)(NewProject));
+export default withRouter(WithData(withStyles(styles)(NewProject)));
--- src/components/popups/new_task.js
+++ src/components/popups/new_task.js
@@ -7,8 +7,11 @@ import {
   Snackbar,
   DialogActions,
   DialogTitle,
+  DialogContent,
   TextField,
   Select,
+  FormControl,
+  InputLabel,
   MenuItem,
   Dialog,
   Button
@@ -16,25 +19,28 @@ import {
 import {graphql, compose} from 'react-apollo';
 import {gql} from 'apollo-boost';
 import * as _ from "lodash";
-import { withStyles } from 'material-ui/styles';
+import {withStyles} from 'material-ui/styles';
 import qs from 'query-string';
 import {withRouter} from 'react-router-dom';
 
 const styles = theme => ({
-  fab: {
+  fab     : {
     position: 'absolute',
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 2,
+    bottom  : theme.spacing.unit * 2,
+    right   : theme.spacing.unit * 2,
   },
-  flex: {
+  flex    : {
     flex: 1,
   },
-  paper: {
+  paper   : {
     width: 330
   },
   centered: {
     textAlign: 'center'
-  }
+  },
+  formControl: {
+    minWidth: 120,
+  },
 });
 
 const getStatusesQuery = graphql(gql`
@@ -84,11 +90,11 @@ class NewTask extends Component {
         projectId: parseInt(qs.parse(this.props.location.search).project_id, 10)
       }
     })
-      .then(({data}) => {
-        this.setState({loading: false, error: ''});
-        this.props.onClose();
-      }).catch((error) => {
-        this.setState({loading: false, error});
+        .then(({data}) => {
+          this.setState({loading: false, error: ''});
+          this.props.onClose();
+        }).catch((error) => {
+      this.setState({loading: false, error});
     });
 
   };
@@ -108,78 +114,92 @@ class NewTask extends Component {
   }
 
   render() {
+    const {classes} = this.props;
     const {name, type, description, status} = this.state;
-    const statuses = _.get(this.props, 'data.projectById.statuses');
-    const statusOptions = _.reduce(statuses, (acc, s) => {
+    const statuses                          = _.get(this.props, 'data.projectById.statuses');
+    const statusOptions                     = _.reduce(statuses, (acc, s) => {
       acc.push({
-        key: s.index,
+        key  : s.index,
         value: s.id,
-        text: s.name
+        text : s.name
       });
       return acc;
     }, []);
     return (
       <Dialog open={this.props.open} maxWidth="md" onClose={this.props.onClose}>
         <DialogTitle id="confirmation-dialog-title">Create new Task</DialogTitle>
-        {/*loading={this.state.loading}*/}
-        <form onSubmit={this.handleSubmit}>
-          {this.state.error && <Snackbar
-            error
-            header='Error occurred'
-            content={<div>this.state.error</div>}
-          />}
-          <TextField
-            label={"Name"}
-            name='name'
-            value={name}
-            placeholder='Name of the task'
-            onChange={this.handleChange('name')}
-          />
-          <TextField
-            label={"Description"}
-            name='description'
-            value={description}
-            placeholder='Describe the task objectives'
-            onChange={this.handleChange('description')}
-          />
-          <Select
-            label={"Type"}
-            name='type'
-            value={type}
-            // options={[
-            //   {key: 0, value: 0, text: "Type 1"},
-            //   {key: 1, value: 1, text: "Type 2"},
-            //   {key: 2, value: 2, text: "Type 3"},
-            //   {key: 3, value: 3, text: "Type 4"}
-            // ]}
-            placeholder='The type of task'
-            onChange={this.handleChange('type')}
-          >
-            {_.map([1, 2, 3, 4], (st, key) =>
-              <MenuItem value={st} key={key}>{'Type '+st}</MenuItem>
-            )}
-          </Select>
-          <Select
-            label={"Status"}
-            name='status'
-            value={status}
-            options={statusOptions}
-            placeholder='The default status of the task'
-            onChange={this.handleChange('status')}
-          >
-            {_.map(statuses, (st, key) =>
-              <MenuItem value={st.id} key={key}>{st.name}</MenuItem>
-            )}
-          </Select>
-        </form>
-        <DialogActions>
-          <Button onClick={this.props.onClose} color="secondary">
-            Cancel
-          </Button>
-          <Button onClick={this.handleSubmit} color="primary">
-            Ok
-          </Button>
-        </DialogActions>
+        {this.state.loading ?
+          <div>Loading...</div>
+          :
+          <DialogContent>
+            <form onSubmit={this.handleSubmit}>
+              {this.state.error && <Snackbar
+                error
+                header='Error occurred'
+                content={<div>this.state.error</div>}
+              />}
+              <TextField
+                fullWidth
+                label={"Name"}
+                name='name'
+                value={name}
+                placeholder='Name of the task'
+                onChange={this.handleChange('name')}
+              />
+              <TextField
+                multiline
+                fullWidth
+                label={"Description"}
+                name='description'
+                value={description}
+                placeholder='Describe the task objectives'
+                onChange={this.handleChange('description')}
+              />
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="type-select">Type</InputLabel>
+                <Select
+                  label={"Type"}
+                  name='type'
+                  value={type}
+                  placeholder='The type of task'
+                  onChange={this.handleChange('type')}
+                  inputProps={{
+                    name: 'type',
+                    id: 'type-select',
+                  }}
+                >
+                  {_.map([1, 2, 3, 4], (st, key) =>
+                    <MenuItem value={st} key={key}>{'Type ' + st}</MenuItem>
+                  )}
+                </Select>
+              </FormControl>
+              <br/>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="type-select">Status</InputLabel>
+                <Select
+                  label={"Status"}
+                  name='status'
+                  value={status}
+                  options={statusOptions}
+                  placeholder='The default status of the task'
+                  onChange={this.handleChange('status')}
+                >
+                  {_.map(statuses, (st, key) =>
+                    <MenuItem value={st.id} key={key}>{st.name}</MenuItem>
+                  )}
+                </Select>
+              </FormControl>
+            </form>
+            < DialogActions>
+              < Button onClick={this.props.onClose} color="secondary">
+                Cancel
+              </Button>
+              <Button onClick={this.handleSubmit} color="primary">
+                Ok
+              </Button>
+            </DialogActions>
+          </DialogContent>
+        }
       </Dialog>
     );
   }
