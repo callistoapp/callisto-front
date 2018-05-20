01aa7d98cec3193f51c7116bfe21c5f7c4273441
cmondion <clement.mondion@clicrdv.com>
2018-05-20 22:26:03 +0200

    feat(releases): implement a simple logic for releases

---
+++ src/components/popups/new_release.js
@@ -0,0 +1,75 @@
+/**
+ * Created by clementmondion on 16/12/2017.
+ */
+
+import React, {Component} from 'react';
+import {
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  Dialog,
+  Button
+} from 'material-ui'
+import {graphql, Mutation} from 'react-apollo';
+import {gql} from 'apollo-boost';
+import qs from 'query-string';
+import {withRouter} from 'react-router-dom';
+
+const CREATE_RELEASE = gql`
+  mutation CreateRelease($version: String!, $projectId: Int!) {
+    createRelease(version: $version, projectId: $projectId) {
+      id
+    }
+  }
+`;
+
+class NewRelease extends Component {
+  handleChange = name => e => this.setState({[name]: e.target.value});
+
+  constructor(props) {
+    super(props);
+    this.state        = {
+      version: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    const projectId = parseInt(qs.parse(this.props.location.search).project_id, 10);
+
+    return (
+      <Dialog open={this.props.open} maxWidth="md" onClose={this.props.onClose}>
+        <DialogTitle id="confirmation-dialog-title">Create new release</DialogTitle>
+        <DialogContent>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              fullWidth
+              label={"Version"}
+              name='version'
+              value={this.state.version}
+              placeholder='Version of the release'
+              onChange={this.handleChange('version')}
+            />
+          </form>
+          < DialogActions>
+            < Button onClick={this.props.onClose} color="secondary">
+              Cancel
+            </Button>
+            <Mutation mutation={CREATE_RELEASE}>
+              {(mutation, {receivedData}) => (
+                <Button size="small" color="primary" onClick={() => {
+                  mutation({variables: {...this.state, projectId}})
+                }}>
+                  Ok
+                </Button>
+              )}
+            </Mutation>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+}
+
+export default withRouter(NewRelease);
--- src/views/releases.js
+++ src/views/releases.js
@@ -1,11 +1,74 @@
 import React from 'react';
+import {Query} from 'react-apollo';
+import {gql} from 'apollo-boost';
+import qs from "query-string";
+import * as _ from 'lodash';
+import {Button, List, ListItem, ListItemText, withStyles} from "material-ui";
+import {Add} from "@material-ui/icons";
+import NewRelease from '../components/popups/new_release'
+
+const GET_RELEASES = gql`
+  query GetReleases($projectId: Int!) {
+    releasesForProject(projectId: $projectId){
+      id,
+      version
+    }
+  }
+`;
+
+
+const styles = theme => ({
+  fab     : {
+    position: 'absolute',
+    bottom  : theme.spacing.unit * 2,
+    right   : theme.spacing.unit * 2,
+  }
+});
+
 
 class Releases extends React.Component {
+  state = {
+    newOpen: false
+  };
   render() {
+    const projectId = parseInt(qs.parse(this.props.location.search).project_id, 10);
+    if (!projectId)
+      return "No project Specified";
     return (
-      <div>Hello From Releases</div>
+      <Query query={GET_RELEASES} variables={{projectId}}>
+        {({loading, error, data}) => {
+          if (loading) return null;
+          if (error) return `Error!: ${error}`;
+          return (
+            <div>
+              {
+                _.isEmpty(data.releasesForProject) ?
+                  "No releases" :
+                  <List>
+                    {_.map(data.releasesForProject, release =>
+                      <ListItem key={release.id} button>
+                        <ListItemText primary={release.version}/>
+                      </ListItem>
+                    )}
+                  </List>
+              }
+              <Button
+                variant="fab"
+                color="primary"
+                onClick={() => this.setState({newOpen: true})}
+                aria-label="add"
+                className={this.props.classes.fab}
+              >
+                <Add/>
+              </Button>
+              <NewRelease open={this.state.newOpen}
+                          onClose={() => this.setState({newOpen: false})}/>
+            </div>
+          )
+        }}
+      </Query>
     )
   }
 }
 
-export default Releases;
+export default withStyles(styles)(Releases);
