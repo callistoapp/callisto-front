de129184fa98559a07d802107fe3d05a360f6032
cmondion <clement.mondion@clicrdv.com>
2018-05-20 22:28:23 +0200

    feat(dashboard): minimalist dashboard

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -3,6 +3,7 @@ import qs from 'query-string';
 import {Query} from 'react-apollo';
 import {gql} from 'apollo-boost';
 import {withRouter} from 'react-router';
+import {CircularProgress, withStyles, Card, Typography, Grid} from 'material-ui';
 import * as _ from 'lodash';
 
 const GET_PROJECT = gql`
@@ -19,12 +20,43 @@ const GET_PROJECT = gql`
         id,
         index
       }
+      releases {
+        id
+      }
     }
   }
 `;
 
+const styles = theme => ({
+  doneDiv: {
+    backgroundColor: "white",
+    height: 200,
+    borderRadius: 4,
+    display: "flex",
+    alignItems: "center",
+  },
+  textDiv: {
+    margin: "auto",
+  },
+  progressDiv: {
+    margin: "auto",
+    position: "absolute"
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 500,
+    marginBottom: 0,
+  },
+  subText: {
+    fontSize: 20,
+    color: "grey",
+    fontWeight: 200,
+    textAlign: "center"
+  }
+});
 
-const Dashboard = ({location}) => {
+
+const Dashboard = ({location, classes}) => {
   const projectId = parseInt(qs.parse(location.search).project_id, 10);
   if (!projectId)
     return "No project Specified";
@@ -33,14 +65,66 @@ const Dashboard = ({location}) => {
       {({ loading, error, data }) => {
         if (loading) return null;
         if (error) return `Error!: ${error}`;
-        const {statuses, name, tasks} = data.projectById;
+        const {statuses, name, tasks, releases} = data.projectById;
         const maxStatus = _.get(_.find(statuses, {index: _.max(_.map(statuses, 'index'))}), 'id');
         const finished = _.filter(tasks, {statusId: maxStatus});
         const done = finished.length * 100 / tasks.length;
         return (
           <div>
             <h2>{name}</h2>
-            {done || 0} %
+            <Grid container justify="center" spacing={8}>
+              <Grid item xs={4}>
+                <Card className={classes.doneDiv}>
+                  <div className={classes.textDiv}>
+                    <div>
+                      <div>
+                        <Typography className={classes.text}>{done || 0}%</Typography>
+                        <div>
+                          <Typography className={classes.subText}>Done</Typography>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={classes.progressDiv}>
+                    <CircularProgress
+                      className={classes.progress}
+                      variant="static"
+                      value={done}
+                      size={200}
+                      thickness={2}
+                    />
+                  </div>
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card className={classes.doneDiv}>
+                  <div className={classes.textDiv}>
+                    <div>
+                      <div>
+                        <Typography className={classes.text}>16</Typography>
+                        <div>
+                          <Typography className={classes.subText}>Tasks remaining</Typography>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card className={classes.doneDiv}>
+                  <div className={classes.textDiv}>
+                    <div>
+                      <div>
+                        <Typography className={classes.text}>{releases.length}</Typography>
+                        <div>
+                          <Typography className={classes.subText}>Releases</Typography>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Card>
+              </Grid>
+            </Grid>
           </div>
         );
       }}
@@ -48,4 +132,4 @@ const Dashboard = ({location}) => {
   )
 };
 
-export default withRouter(Dashboard);
+export default withStyles(styles)(withRouter(Dashboard));
