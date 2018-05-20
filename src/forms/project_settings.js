4d276b19b3f16ce853577c2077c21866b4e64a2c
cmondion <clement.mondion@clicrdv.com>
2018-05-20 22:27:17 +0200

    feat(settings form): externalize form for settings

---
+++ src/forms/project_settings.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {TextField} from "material-ui";
+
+const ProjectForm = (props) => {
+  const {name, description, repository, url, handleChange} = props;
+  return (
+    <form onSubmit={this.handleSubmit}>
+      <TextField
+        fullWidth
+        label={'Name'}
+        name='name'
+        value={name}
+        placeholder='Name of the project'
+        onChange={handleChange('name')}
+      />
+      <TextField
+        fullWidth
+        label={'Description'}
+        name='description'
+        value={description}
+        placeholder='Describe the project objectives'
+        onChange={handleChange('description')}
+      />
+      <TextField
+        fullWidth
+        label={'Repository'}
+        name='repository'
+        value={repository}
+        placeholder='The Url of the repository hosing the source code'
+        onChange={handleChange('repository')}
+      />
+      <TextField
+        fullWidth
+        label={'Url'}
+        name='url'
+        value={url}
+        placeholder='The url of the desired website'
+        onChange={handleChange('url')}
+      />
+    </form>
+  )
+};
+
+export default ProjectForm;
+
