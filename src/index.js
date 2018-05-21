e470a3f149e0018b0faa317636f37e95b928fa5e
cmondion <clement.mondion@clicrdv.com>
2018-05-21 13:10:17 +0200

    feat(hot module): add hot module replacement without ejecting

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { AppContainer } from 'react-hot-loader';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <App/>,
+// Wrap the rendering in a function:
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <App/>
+    </AppContainer>,
     document.getElementById('root')
-);
+  );
+}
+
 registerServiceWorker();
+
+// First render
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
